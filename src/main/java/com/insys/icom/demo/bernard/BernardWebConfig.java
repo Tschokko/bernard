742cbe7c9c8e39f924f5617516a9217a1228f18f
Djordje Gladovic <djordje.gladovic@qskills.de>
2018-04-27 15:25:13 +0200

    Added CORS support

--- src/main/java/com/insys/icom/demo/bernard/BernardWebConfig.java
+++ src/main/java/com/insys/icom/demo/bernard/BernardWebConfig.java
@@ -1,15 +1,30 @@
 package com.insys.icom.demo.bernard;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class BernardWebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/webjars/**")
                 .addResourceLocations("/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .maxAge(3600)
+                .allowedHeaders("Accept", "Content-Type", "Origin",
+                        "Authorization", "X-Auth-Token")
+                .exposedHeaders("X-Auth-Token", "Authorization")
+                .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS", "HEAD");
     }
 }
